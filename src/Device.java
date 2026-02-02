e42b9cbc9690321f4b8fad47b2b43d7e997d83f7
Jackson "J.D <jackson.david.otis@gmail.com>
2026-02-02 14:03:05 -0500

    refactor Device class and implement Host class for packet handling

--- src/Device.java
+++ src/Device.java
@@ -1,95 +1,13 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.DatagramSocket;
-import java.net.SocketException;
-import java.net.DatagramPacket;
-import java.util.Scanner;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 public class Device {
 
-    public static String id;
-    public static int port;
-    public static String ip;
-
-    public static void main(String[] args) throws SocketException {
-        id = args[0];
-        ip = "127.0.0.1";
-        port = 3094;
-        System.out.println();
-        ExecutorService es = Executors.newFixedThreadPool(2);
-        //needs a way to grab info from config file
-        //Then send the info to the multithreading
-        es.submit(new SendPacket(id, ip, port, es));
-        es.submit(new ReceivePacket());
-    }
-
-    public static void getIPPort(String id) {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-    }
-
+    public String id;
+    public int port;
+    public String ip;
 
     public Device(String id, String ip, int port) {
         this.id = id;
         this.port = port;
         this.ip = ip;
-    }
-
-
-    public static class SendPacket implements Runnable {
-
-        private String id;
-        private String ip;
-        private int port;
-        private ExecutorService es;
-
-        public SendPacket(String id, String ip, int port, ExecutorService es) {
-            this.id = id;
-            this.ip = ip;
-            this.port = port;
-            this.es = es;
-        }
-
-        @Override
-        public void run() {
-            Scanner scanner = new Scanner(System.in);
-            while (true) {
-                System.out.print("Enter the Address of the recipient and the message\nyou would like to send (ex: A:HelloWorld!)\nTo quit the device, type 'quit': ");
-                String message = scanner.nextLine();
-                if (message.toLowerCase().equals("quit")) {
-                    System.out.println("\nShutting down device\n");
-                    es.shutdown();
-                    break;
-                } else {
-                    String[] messageArray = message.split(":", 2);
-                    String sendID = messageArray[0];
-                    String sendMessage = messageArray[1];
-                    System.out.println(sendID);
-                    System.out.println(sendMessage);
-                }
-            }
-        }
-    }
-
-    public static class ReceivePacket implements Runnable {
-
-
-        public ReceivePacket() throws SocketException {
-        }
-
-        @Override
-        public void run() {
-            int port_Test = 1;
-            try {
-                DatagramSocket deviceSocket = new DatagramSocket(port_Test);
-            } catch (SocketException e) {
-                throw new RuntimeException(e);
-            }
-            DatagramPacket dataRequest = new DatagramPacket(new byte[1024], 1024);
-        }
-
-
     }
 
 }
---
+++ src/Host.java
@@ -0,0 +1,118 @@
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.*;
+
+public class Host {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("id improperly specified");
+            return;
+        }
+        String hostID = args[0];
+        try {
+            Parser.parse("src/Config.txt");
+            Device myDevice = Parser.devices.get(hostID);
+            if (myDevice == null) {
+                System.out.println("Device ID " + hostID + " not found in config file");
+                return;
+            }
+            List<String> neighborID = Parser.links.get(hostID);
+            if (neighborID.isEmpty()) {
+                System.out.println("No neighbor found for host " + hostID);
+                return;
+            }
+            Device neighbor = Parser.devices.get(neighborID.get(0)); // assuming only one neighbor for host
+
+            // create thread pool of 2 threads
+            ExecutorService es = Executors.newFixedThreadPool(2);
+
+            // Start send and receive threads
+            es.execute(new SendPacket(myDevice.id, neighbor.ip, neighbor.port, es));
+            es.execute(new ReceivePacket(myDevice.port, es));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static class SendPacket implements Runnable {
+
+        private String id;
+        private String ip;
+        private int port;
+        private ExecutorService es;
+
+        public SendPacket(String id, String ip, int port, ExecutorService es) {
+            this.id = id;
+            this.ip = ip;
+            this.port = port;
+            this.es = es;
+        }
+
+        @Override
+        public void run() {
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                System.out.print("Enter the Address of the recipient and the message\nyou would like to send (ex: A:HelloWorld!)\nTo quit the device, type 'quit': ");
+                String message = scanner.nextLine();
+                if (message.trim().toLowerCase().equals("quit")) {
+                    System.out.println("\nShutting down device\n");
+                    scanner.close();
+                    es.shutdownNow(); // signals other thread to stop, too.
+                    break;
+                } else {
+                    String[] messageArray = message.split(":", 2);
+                    String sendID = messageArray[0];
+                    String sendMessage = messageArray[1];
+
+                    byte[] buffer = (id + ":" + sendID + ":" + sendMessage).getBytes();
+                    try {
+                        DatagramSocket socket = new DatagramSocket();
+                        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, InetAddress.getByName(ip), port);
+                        socket.send(packet);
+                        socket.close();
+                    } catch (IOException e) {
+                        System.err.println("Error sending packet: " + e.getMessage());
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    public static class ReceivePacket implements Runnable {
+
+        private int port;
+        private ExecutorService es;
+
+        public ReceivePacket(int port, ExecutorService es) {
+            this.port = port;
+            this.es = es;
+        }
+
+        @Override
+        public void run() {
+            try  {
+                DatagramSocket hostSocket = new DatagramSocket(port);
+                byte[] buffer = new byte[1024];
+                while (!es.isShutdown()) { // keep running until quit
+                    DatagramPacket dataRequest = new DatagramPacket(buffer, buffer.length);
+                    hostSocket.receive(dataRequest);
+                    String received = new String(dataRequest.getData(), 0, dataRequest.getLength());
+                    System.out.println("\nReceived: " + received);
+                }
+                hostSocket.close();
+            } catch (IOException e) {
+                System.err.println("Error receiving packet: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+}
